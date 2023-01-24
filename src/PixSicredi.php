87385694c4f39accb42285b64f3332631baadd0b
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-23 21:21:11 -0400

    getUrlWebhook

--- src/PixSicredi.php
+++ src/PixSicredi.php
@@ -94,4 +94,26 @@ class PixSicredi
 		curl_close($curl);
 		return $response;
 	}
+
+	public function getUrlWebhook()
+	{
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL =>  $this->url . '/api/v2/webhook/',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+			CURLOPT_HTTPHEADER => array('Authorization: Bearer  ' . $this->token . ' '),
+		));
+
+		$response = curl_exec($curl);
+		$status   = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		curl_close($curl);
+		return $response;
+	}
 }
